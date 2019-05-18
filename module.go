62b8bc11273459366fb706db7c5f2130f2c97c9b
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-18 17:37:31 +0100

    Group package-level variables

--- module.go
+++ module.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-05-09 18:10:45 F5D032                              zr-fs/[module.go]
+// :v: 2019-05-18 17:37:21 57FAC5                              zr-fs/[module.go]
 // -----------------------------------------------------------------------------
 
 // Package fs implements file-system related functions
@@ -12,25 +12,27 @@ import (
 	"github.com/balacode/zr"
 )
 
-// PL is fmt.Println() but is used only for debugging.
-var PL = fmt.Println
+var (
+	// PL is fmt.Println() but is used only for debugging.
+	PL = fmt.Println
 
-// VL is zr.VerboseLog() but is used only for debugging.
-var VL = zr.VerboseLog
+	// VL is zr.VerboseLog() but is used only for debugging.
+	VL = zr.VerboseLog
 
-// Callers returns a human-friendly string showing the call stack with each
-// callers calling method or function's name and line number. The most
-// immediate are shown first, followed by their callers, and so on.
-// For brevity, 'runtime.*', 'syscall.*' and 'testing.*'
-// top-level callers are not included.
-//
-// func Callers(options ...interface{}) string
-var callers = zr.Callers
+	// Callers returns a human-friendly string showing the call stack with
+	// each calling method or function's name and line number. The most
+	// immediate are shown first, followed by their callers, and so on.
+	// For brevity, 'runtime.*', 'syscall.*' and 'testing.*'
+	// top-level callers are not included.
+	//
+	// func Callers(options ...interface{}) string
+	callers = zr.Callers
 
-// Error outputs an error message to the standard output and to a
-// log file named 'run.log' saved in the program's current directory,
-// It also outputs the call stack (names and line numbers of callers.)
-// Returns an error value initialized with the message.
-var myError = zr.Error
+	// Error outputs an error message to the standard output and to a
+	// log file named 'run.log' saved in the program's current directory,
+	// It also outputs the call stack (names and line numbers of callers.)
+	// Returns an error value initialized with the message.
+	myError = zr.Error
+)
 
 //end
